0c930faeb5def0a3fcaac8d0f3a504479c7c587a
yan <freshjs.oftd@gmail.com>
2022-04-02 13:03:25 +0300

    Add bind, call, apply

--- src/lesson-011.js
+++ src/lesson-011.js
@@ -1,90 +1,33 @@
 'use strict';
-// let num = Number('123@');
-// console.log(num);
-// let num1 = parseInt('456');
-// console.log(num1);
-// let str = String(789);
-// console.log(str);
-// let bool = 123;
-// console.log(!!bool);
-// let sum = (2 + 3)/2
-// let sum1 = num === 10 ? 20 : 10
-// if(num && !bool){
-//   console.log(num);
-// }else {
-//   console.log(false);
-// }
-// num1 = num1 + 3;
-// console.log(num1);
-// num1 +=3;
-// console.log(++num1);
-// num1++;
-// num1--;
-// ++num1;
-// console.log(num || !bool || !!num);
-// let num2 = 20;
-// for(let i = 0; i < num2; i++) {
-//   if(i === 15) break;
-//   console.log(i);
-// }
-// const arrow = (a, b) => a + b;
-// console.log(arrow(20, 30));
-// const user = {
-//   // fName: 'Vasya',
-//   lName: 'Pupkin',
-//   age: 30,
-//   'first name': 'Vasya',
-// }
-// user.fName ='Yan';
-// user.gender = 'Male';
-// console.log(user.age);
-// delete user.fName;
-// console.log(user['first name']);
-// for (const key in user) {
-//   console.log(`${key} = ${user[key]}`);
-// }
-// function Person(fName, age){
-//   this.fName = fName;
-//   this.age = age;
-// }
-// const vasya = new Person('Vasya', 25);
-// console.log(vasya);
 
-
-function getObj() {
-	return this;
+function Player(fName, lName, rate, age, city){
+  this.fName = fName;
+  this.lName = lName;
+  this.rate = rate;
+  this.age = age;
+  this.city = city;
 }
-function Country(
-	title = 'Ukraine',
-	capital = 'Kiyv',
-	area = 8e5,
-	population = 4.9e6,
-) {
-  this.title = title;
-  this.capital = capital;
-  this.area = area + ' square km';
-  this.population = population + ' persons';
-  this.getCountry = getObj;
-}
-// ==============================================
-// Inheritance
-// Country.prototype.getCountry = getObj;
-// ==============================================
-
-const ukraine = new Country();
-const usa = new Country('USA', 'Washington', 1e6, 15e6);
-function printCountryPropeties(country){
-  for(let key in country) {
-    if(typeof country[key] !== 'function')
-    console.log(`${key}: ${country.getCountry()[key]}`);
+const maLong = new Player('Ma', 'Long', 3000, 35, 'Odessa');
+const xuXin = new Player('Xu', 'Xin', 2500, 30, 'Kiev');
+const kokiNiva = new Player('Koki', 'Niva', 2000, 25, 'Zaporozhie');
+const players = [maLong, xuXin, kokiNiva];
+function getProps(limAge){
+  if(this.age > limAge){
+    console.log(`${this.fName} ${this.lName} is too mutch old`);
+  }else{
+    for(const key in this){
+      console.log(`${key} = ${this[key]}`);
+    }
   }
 }
-console.log(usa);
+// call(), apply(), bind()
+const limAge = 30;
+for(let i = 0; i < players.length; i++){
+  getProps.apply(players[i], [limAge])
+  console.log('=================');
+}
+const getPropsWithBind = getProps.bind(maLong);
 
-// debugger
-printCountryPropeties(usa);
-console.log('====================');
-printCountryPropeties(ukraine);
-
+getPropsWithBind()
 
 
