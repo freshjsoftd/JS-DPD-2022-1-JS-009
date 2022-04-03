3c8e87fad2df2e3391802db17982f1d2b15228fe
yan <freshjs.oftd@gmail.com>
2022-04-03 13:14:35 +0300

    Add simple methods of array

--- src/lesson-012.js
+++ src/lesson-012.js
@@ -1,28 +1,36 @@
 'use strict';
 
-// let num = 255;
+const arr = [1, 'TSP', null, NaN, Infinity, -Infinity, test, {}];
+const obj = {
+  fName: 'vasya',
+  lName: 'pupkin',
+  age: 30,
+}
+console.log(arr);
+console.log(obj);
+let n = arr[5];
+console.log(n);
 
-// console.log(num.toString(2));
-// console.log(isNaN('NaN'));
-// console.log(isNaN(undefined));
-// console.log(isNaN({}));
-// console.log(isNaN('kuku'));
-// console.log('============================');
-// console.log(Number.isNaN(NaN));
-// console.log(Number.isNaN(undefined));
-// console.log(Number.isNaN(0/0));
-// console.log(Number.isNaN('kuku'));
-let str = 'Stiga';
-let str2 = 'Donic';
-let str3 = 'Butterfly'
-let bigStr = ' I like Iava Script lang '
-console.log(str.concat(str2, str3));
-console.log(str.includes('ig'));
-console.log(str3.repeat(3));
-console.log(str3.slice(3, 1));
-const strArr = bigStr.split('', 5);
-console.log(strArr);
-console.log(bigStr);
-console.log(bigStr.trim());
-console.log(bigStr.toUpperCase().trim());
-console.log(bigStr);
+function test(){
+  return 'test';
+}
+const arr1 = Array(20, 10, 'str');
+console.log(arr1);
+const numArr = [1,3,7,5,9,12];
+console.log(numArr.push(14,16));
+console.log(numArr);
+console.log(numArr.unshift(-1, 0));
+console.log(numArr);
+console.log(numArr.pop());
+console.log(numArr);
+console.log(numArr.shift());
+console.log(numArr);
+
+let str2 = arr.join();
+console.log(str2);
+// console.log(arr.reverse());
+console.log(numArr.sort((a, b) => a - b));
+
+const newArr = arr.splice(2, 4, 25, 'Malong', true);
+console.log(newArr);
+console.log(arr);
