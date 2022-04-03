e652dc14ffd3b0e1425bc0bbc429494683cc1de6
yan <freshjs.oftd@gmail.com>
2022-04-03 12:00:58 +0300

    Add primitives methods

--- src/lesson-012.js
+++ src/lesson-012.js
@@ -1,17 +1,28 @@
 'use strict';
-function findSolution(target){
-  function find(current, history){
-    if(current === target){
-      return history;
-    }else if(current > target){
-      return null;
-    }else{
-      return find(current + 5, `(${history} + 5)`) || 
-             find(current * 3, `${history} * 3`) || 
-             find(current * 2, `${history} * 2`)
-    }
-  }
-  return find(1, '1')
-}
 
-console.log(findSolution(12));
+// let num = 255;
+
+// console.log(num.toString(2));
+// console.log(isNaN('NaN'));
+// console.log(isNaN(undefined));
+// console.log(isNaN({}));
+// console.log(isNaN('kuku'));
+// console.log('============================');
+// console.log(Number.isNaN(NaN));
+// console.log(Number.isNaN(undefined));
+// console.log(Number.isNaN(0/0));
+// console.log(Number.isNaN('kuku'));
+let str = 'Stiga';
+let str2 = 'Donic';
+let str3 = 'Butterfly'
+let bigStr = ' I like Iava Script lang '
+console.log(str.concat(str2, str3));
+console.log(str.includes('ig'));
+console.log(str3.repeat(3));
+console.log(str3.slice(3, 1));
+const strArr = bigStr.split('', 5);
+console.log(strArr);
+console.log(bigStr);
+console.log(bigStr.trim());
+console.log(bigStr.toUpperCase().trim());
+console.log(bigStr);
