29f9eeb825f0c453b625bf9c418b6d8ae18b3c52
yan <freshjs.oftd@gmail.com>
2022-04-09 12:09:15 +0300

    Add arguments

--- src/lesson-013.js
+++ src/lesson-013.js
@@ -1,6 +1,14 @@
-// 'use strict';
-let x = 1;
-y ;
+'use strict';
 
-console.log(`${x} + ${y}`);
-let y;
+function getArg(a){
+  let arg = arguments;
+  return arg;
+}
+console.log(getArg(1,2,5,6, 'str'));
+const arr = [];
+console.log(arr);
+for (const val of getArg(1,2,5,6, 'ste')) {
+  console.log(val);
+}
+const arrArg = Array.from(getArg(1,2,5,6, 'str'));
+console.log(arrArg);
