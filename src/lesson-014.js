75caff57aad56106c01854aa6a452b161320ed12
yan <freshjs.oftd@gmail.com>
2022-04-10 13:46:43 +0300

    Add destucture

--- src/lesson-014.js
+++ src/lesson-014.js
@@ -1,115 +1,56 @@
 'use strict';
-const arr1 = [];
-const arr2 = [];
 
-function MyArray() {
-	this.length = 0;
-	for (let i = 0; i < arguments.length; i++) {
-		this.push(arguments[i]);
-	}
+// let age = Symbol('age');
+// let age1 = Symbol('age');
+
+
+
+// user[age] = 50;
+// console.log(user.age);
+// console.log(user[age]);
+
+// for (const key in user) {
+//   console.log(key);
+//   }
+// console.log(Object.keys(user));
+function getArgs(){
+  return arguments;
 }
-MyArray.prototype = new MyArrayProto();
 
-const myArr1 = new MyArray(1, 3, 'str');
-const myArr2 = new MyArray();
-
-// Object with logic
-function MyArrayProto() {
-  // some
-	this.some = function some(fn) {
-		for (let i = 0; i < this.length; i++) {
-			if (fn(this[i], i, this)) {
-				return true;
-			}
-			return false;
-		}
-	};
-	// push
-	this.push = function push() {
-		if (arguments) {
-			for (let i = 0; i < arguments.length; i++) {
-				this[this.length++] = arguments[i];
-			}
-		}
-		return this.length;
-	};
-
-	// pop
-	this.pop = function pop() {
-		if (this.length === 0) {
-			return;
-		}
-		const lastItem = this[this.length - 1];
-		delete this[--this.length];
-		return lastItem;
-	};
-	// forEach
-	this.forEach = function forEach(fn) {
-		for (let i = 0; i < this.length; i++) {
-			fn(this[i], i, this);
-		}
-	};
-	
-	// filter
-	this.filter = function filter(fn) {
-		const newArray = new MyArray();
-		for (let i = 0; i < this.length; i++) {
-			if (fn(this[i], i, this)) {
-				newArray.push(this[i]);
-			}
-		}
-		return newArray;
-	};
-	// map
-	this.map = function map(fn) {
-		const result = new MyArray();
-		for (let i = 0; i < this.length; i++) {
-			result.push(fn(this[i], i, this));
-		}
-		return result;
-	};
-	
+const user = {
+  fName: 'Vasya',
+  lName: 'Pupkin',
+  age: 30,
 }
-// isArray
-MyArray.isMyArray = function (obj) {
-  return obj instanceof MyArray;
-};
+const person = {
+  fName: 'Petya',
+  lName: 'Pyatov',
+  age: 20}
+
+const getArrowArgs = (...args) => args;
+console.log(getArgs());
+const arrFromArgs = getArrowArgs(1, 56, 'str')
+const arr1 = ['Domic', 'Stiga']
+const sumArray = [...arrFromArgs, ...arr1];
+console.log(sumArray);
+const user1 = {...user};
+
+let {fName: firstName, lName: lastName, age} = user;
+let {fName: personName, lName: pesonName, age: pAge} = person;
 
 
-// HW country constructor
-function getObj() {
-	return this;
-}
-function Country(
-	title = 'Ukraine',
-	capital = 'Kiyv',
-	area = 8e5,
-	population = 4.9e6,
-) {
-  this.title = title;
-  this.capital = capital;
-  this.area = area + ' square km';
-  this.population = population + ' persons';
-  // this.getCountry = function(){
-  //   return this.capital
-  // };
-}
-// ==============================================
-// Inheritance
-Country.prototype.getCountry = getObj;
-// ==============================================
+let [oneTr, two,,three] = sumArray;
 
-const ukraine = new Country();
-const usa = new Country('USA', 'Washington', 1e6, 15e6);
-function printCountryPropeties(country){
-  for(let key in country) {
-    if(typeof country[key] !== 'function')
-    console.log(`${key}: ${country.getCountry()[key]}`);
-  }
-}
-console.log(usa);
+console.log(firstName, lastName, age);
+console.log(personName, pesonName, pAge);
+console.log(two, oneTr,  three);
 
-// debugger
-printCountryPropeties(usa);
-console.log('====================');
-printCountryPropeties(ukraine);
+let x = 20;
+let y = 30;
+[x, y] = [y, x];
+
+// let temp = x;
+// x = y;
+// y = 20;
+console.log(x, y);
+console.log(`X + Y = ${x + y}`);
