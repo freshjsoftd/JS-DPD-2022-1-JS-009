9bbcbbaed6b547d2316c257281a5db4bdbaab27f
yan <freshjs.oftd@gmail.com>
2022-05-07 12:58:24 +0300

    Add code for XHR

--- src/lesson-020-async.js
+++ src/lesson-020-async.js
@@ -1,24 +1,11 @@
 'use strict';
 
-// const promise = new Promise((resolve, reject) => {
-//   setTimeout(() => resolve('Is done'), 2000);
-//   // setTimeout(() => reject('Something went wrong'), 2000);
-// })
-// .then((value) => value)//
-// .then((result) => result)
-// .finally(console.log('The end'))
-// .then((res) => console.log(`${res}$`))
-// .catch((err) => console.log(err))
-// console.log(promise);
-
-
-function sum(a, b){
-  return new Promise((resolve) => {
-    const result = a + b;
-    resolve(result);
-  })
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.responseType = 'application/json';
+xhr.send();
+xhr.onload = () => {
+  console.log(xhr.response);
 }
-console.log(sum(5,7));
-sum(5, 7)
-  .then((value) => value + '$')
-  .then((res) => console.log(res))
+
