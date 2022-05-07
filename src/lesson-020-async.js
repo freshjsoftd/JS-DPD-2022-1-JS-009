64f3aec0318b5255e626e9a4922315227d8895fa
yan <freshjs.oftd@gmail.com>
2022-05-07 14:06:55 +0300

    Add code for fetch api

--- src/lesson-020-async.js
+++ src/lesson-020-async.js
@@ -1,11 +1,15 @@
 'use strict';
 
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.responseType = 'application/json';
-xhr.send();
-xhr.onload = () => {
-  console.log(xhr.response);
-}
+const myRequest = new Request('https://jsonplaceholder.typicode.com/todos');
+fetch(myRequest)
+      .then((response) => {
+        if(response.ok){
+          console.log('Ok');
+        }
+        response.json()
+      })
+      .then((jsonResponse) => localStorage.setItem('todos', JSON.stringify(jsonResponse)))
+
+// const todos = JSON.parse(localStorage.getItem('todos'));
+// console.log(todos);
 
