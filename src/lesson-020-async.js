08acf2cc3905fa142bed0d540ae162c91e32a6cd
yan <freshjs.oftd@gmail.com>
2022-05-08 13:52:56 +0300

    Add final code for lesson@021

--- src/lesson-020-async.js
+++ src/lesson-020-async.js
@@ -1,15 +1,46 @@
 'use strict';
 
-const myRequest = new Request('https://jsonplaceholder.typicode.com/todos');
-fetch(myRequest)
-      .then((response) => {
-        if(response.ok){
-          console.log('Ok');
-        }
-        return response.json();
-      })
-      .then((jsonResponse) => localStorage.setItem('todos', JSON.stringify(jsonResponse)))
+// const myRequest = new Request('https://jsonplaceholder.typicode.com/todos');
+// fetch(myRequest)
+//       .then((response) => {
+//         if(response.ok){
+//           console.log('Ok');
+//         }
+//         return response.json();
+//       })
+//       .then((jsonResponse) => localStorage.setItem('todos', JSON.stringify(jsonResponse)))
+//       .catch((error) => {
+//         console.log(error);
+//       })
 
-const todos = JSON.parse(localStorage.getItem('todos'));
-console.log(todos);
+// const todos = JSON.parse(localStorage.getItem('todos'));
+// console.log(todos);
 
+// function sum(a,b){
+//   return new Promise((resolve) =>{
+//     const result = a + b;
+//     resolve(result);
+//   })
+// }
+const button = document.querySelector('button');
+async function asyncFetch() {
+	try {
+		const response = await fetch(
+			'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
+		);
+		const currency = await response.json();
+		localStorage.setItem('currency', JSON.stringify(currency));
+		console.log(currency);
+	} catch (error) {
+		console.log(error.name);
+		console.log(error.message);
+	} finally {
+		console.log('Hello');
+	}
+}
+// asyncFetch();
+button.addEventListener('click', asyncFetch);
+// localStorage.clear();
+const date = new Date().toLocaleDateString('us');
+console.log(date);
+button.textContent = date
---
+++ src/lesson-021-task.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const form = document.querySelector('form');
+const inputBuy = document.querySelector('#input-buy');
+const inputSale = document.querySelector('#input-sale');
+const curChoiceRButton = Array.from(document.getElementsByName('curr'));
+// let date = new Date().toLocaleDateString('uk');
+
+// fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+// .then((response) => response.json())
+// .then((res) => localStorage.setItem('currency', JSON.stringify(res)));
+let date = new Date().toLocaleDateString('uk');
+const getCurrencyPBank = async () => {
+  const response = await fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+  const currency = await response.json();
+  localStorage.setItem(date, JSON.stringify(currency));
+}
+getCurrencyPBank();
+console.log(date);
+
+const currencies = JSON.parse(localStorage.getItem(date));
+// console.log(currencies);
+function setCurrencyToInput(){
+  let radioValue = '';
+  curChoiceRButton.forEach((radio) => {
+    if (radio.checked){
+      radioValue = radio.value;
+    }
+  })
+  currencies.forEach((currency) => {
+    if(radioValue.toLowerCase() === currency['ccy'].toLowerCase()){
+      inputBuy.value = currency['buy'];
+      inputSale.value = currency['sale'];
+    }
+  })
+  // console.log(radioValue);
+  console.log(currencies);
+}
+form.addEventListener('click', setCurrencyToInput);
