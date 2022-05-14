601f48334e5b13e098959729b8dabfe3681c1df7
yan <freshjs.oftd@gmail.com>
2022-05-14 14:07:54 +0300

    Add final code for lesson#022

--- src/lesson-022-http.js
+++ src/lesson-022-http.js
@@ -1,144 +1,27 @@
 'use strict';
-// POST
-const postRequest = async () => {
-	const url = 'https://jsonplaceholder.typicode.com/users';
-	const body = {
-		name: 'John Doe',
-		username: 'John',
-		email: 'john@typicode.com',
-	};
-	const headers = new Headers();
-	headers.append('Content-Type', 'application/json');
-	const init = {
-		method: 'POST',
-		body: JSON.stringify(body),
-		headers: headers,
-	};
-	const request = new Request(url, init);
-	try {
-		const response = await fetch(request);
-		if (!response.ok) throw new Error(response.statusText);
-		const json = await response.json();
-		console.log(json.id);
-	} catch (error) {
-    console.log(error);
-  }
-};
-postRequest();
-// PUT
-const putRequest = async () => {
-	const url = 'https://jsonplaceholder.typicode.com/users/1';
-	const body = {
-		name: 'Jane Smith',
-		username: 'Jane',
-		email: 'jane@typicode.com',
-	};
-	const headers = new Headers();
-	headers.append('Content-Type', 'application/json');
-	const init = {
-		method: 'PUT',
-		body: JSON.stringify(body),
-		headers: headers,
-	};
-	const request = new Request(url, init);
-	try {
-		const response = await fetch(request);
-		if (!response.ok) throw new Error(response.statusText);
-		const json = await response.json();
-		console.log(json);
-	} catch (error) {
-    console.log(error);
-  }
-};
-putRequest();
-// PATCH
-const patchRequest = async () => {
-	const url = 'https://jsonplaceholder.typicode.com/users/1';
-	const body = {
-    name: 'Jane Smith',
-		email: 'jane-smith@typicode.com',
-	};
-	const headers = new Headers();
-	headers.append('Content-Type', 'application/json');
-	const init = {
-		method: 'PATCH',
-		body: JSON.stringify(body),
-		headers: headers,
-	};
-	const request = new Request(url, init);
-	try {
-		const response = await fetch(request);
-		if (!response.ok) throw new Error(response.statusText);
-		const json = await response.json();
-		console.log(json);
-	} catch (error) {
-    console.log(error);
-  }
-};
-patchRequest();
-// DELETE
-const delRequest = async () => {
-	const url = 'https://jsonplaceholder.typicode.com/users/2';
-	const request = new Request(url);
-	try {
-		const response = await fetch(request);
-		if (!response.ok) throw new Error(response.statusText);
-		// const json = await response.json();
-		console.log(response);
-	} catch (error) {
-    console.log(error);
-  }
-};
-delRequest();
-// GET
-const getRequest = async () => {
-	const url = 'https://jsonplaceholder.typicode.com/users/';
-	
-	const headers = new Headers();
-	headers.append('Content-Type', 'application/json');
-	const init = {
-		method: 'GET',
-		headers: headers,
-	};
-	const request = new Request(url, init);
-	try {
-		const response = await fetch(request);
-		if (!response.ok) throw new Error(response.statusText);
-		const json = await response.json();
-    localStorage.setItem('users', JSON.stringify(json));
-		console.log(json);
-	} catch (error) {
-    console.log(error);
-  }
-};
-getRequest();
-const users = JSON.parse(localStorage.getItem('users'));
-console.log(users);
-// Filter albums by username
-const filterRequest = async () => {
-	const url = 'https://jsonplaceholder.typicode.com/albums?userId=1';
-	const request = new Request(url);
-	try {
-		const response = await fetch(request);
-		if (!response.ok) throw new Error(response.statusText);
-		const json = await response.json();
-		console.log(json);
-	} catch (error) {
-    console.log(error);
-  }
-};
-filterRequest();
-// Nested resources
-const nestedRequest = async () => {
-	const url = 'https://jsonplaceholder.typicode.com/albums/2/photos';
-	const request = new Request(url);
-	try {
-		const response = await fetch(request);
-		if (!response.ok) throw new Error(response.statusText);
-		const json = await response.json();
-		console.log(json);
-	} catch (error) {
-    console.log(error);
-  }
-};
-nestedRequest();
+const img = document.getElementById('icon');
+// console.log(img);
+
+fetch('../public/assets/imgs/GOT.jpg')
+    .then(response => {
+      if(!response.ok) throw new Error(`Error status is ${response.statusText}`);
+      return response.blob();
+    })
+    .then(response => {
+      img.src = URL.createObjectURL(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    // const ukrLid = 'Zelensky';
+    // const input = document.queryCommandIndeterm()
+    // const liderName = input.value;
+    // let url = '';
+
+    // switch(liderName.toLocaleLowerCase()){
+    //   case ukrLid.toLowerCase(): {
+    //         url = '';
+    //         break;
+    //   }
+    //   default: url = ''
+    // }
