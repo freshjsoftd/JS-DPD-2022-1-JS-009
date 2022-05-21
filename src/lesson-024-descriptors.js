0de82bde31013d4cbee2a03c2c5bb51b8524d1c7
yan <freshjs.oftd@gmail.com>
2022-05-21 12:14:48 +0300

    Add code for freeze and others methods

--- src/lesson-024-descriptors.js
+++ src/lesson-024-descriptors.js
@@ -38,32 +38,44 @@ const flags = {
 }
 
 const markiz = new Dogs(5, 'Markiz', 20, 4, 'Pudel');
-Object.defineProperty(markiz, 'breed', flags);
-Object.defineProperty(markiz, 'say', flags);
-// Object.defineProperty(markiz, 'smart', {value: 'true',
-//                                        writable: true, 
-//                                        enumerable: true,
-//                                       configurable: true});
-Object.defineProperties(markiz, {
-  smart: {value: true, writable: true},
-  height: {value: 30, enumerable: true }
-})
-//  delete markiz.breed;
-console.log(markiz);
-for (const prop in markiz) {
+// Object.defineProperty(markiz, 'breed', flags);
+// Object.defineProperty(markiz, 'say', flags);
+// // Object.defineProperty(markiz, 'smart', {value: 'true',
+// //                                        writable: true, 
+// //                                        enumerable: true,
+// //                                       configurable: true});
+// Object.defineProperties(markiz, {
+//   smart: {value: true, writable: true},
+//   height: {value: 30, enumerable: true }
+// })
+// //  delete markiz.breed;
+// console.log(markiz);
+// for (const prop in markiz) {
   
-    console.log(`${prop} is ${markiz[prop]}`);
+//     console.log(`${prop} is ${markiz[prop]}`);
   
-}
+// }
 const obj = {
   fName: 'vas',
   age: 25,
 }
-console.log(Object.getOwnPropertyDescriptor(markiz, 'breed'));
+// console.log(Object.getOwnPropertyDescriptor(markiz, 'breed'));
 
-const mark1 = Object.assign({}, markiz);
-console.log(mark1);
-console.log(Object.getOwnPropertyDescriptor(mark1, 'breed'));
-const mark2 = Object.defineProperties({}, 
-      Object.getOwnPropertyDescriptors(markiz));
-console.log(Object.getOwnPropertyDescriptors(mark2));
+// const mark1 = Object.assign({}, markiz);
+// console.log(mark1);
+// console.log(Object.getOwnPropertyDescriptor(mark1, 'breed'));
+// const mark2 = Object.defineProperties({}, 
+//       Object.getOwnPropertyDescriptors(markiz));
+// console.log(Object.getOwnPropertyDescriptors(mark2));
+
+const arr = Object.create(new Dogs(), {breed: {value: 'dog', writable:true},
+            age: {value: 17}});
+const arr1 = []
+console.log(obj);
+console.log(Object.getOwnPropertyDescriptors(arr));
+// Object.preventExtensions(markiz);
+Object.seal(markiz);
+// markiz.nik = 'Sobak'
+// delete markiz.age
+markiz.age = 2
+
