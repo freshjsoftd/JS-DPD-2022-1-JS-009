9c2d0cf6e70f5dae178813e33c1e4f8c726714a3
yan <freshjs.oftd@gmail.com>
2022-05-21 11:23:52 +0300

    Add code for descriptors

---
+++ src/lesson-024-descriptors.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// //Create data for testing
+class Predators {
+	constructor(age, name, weight, limbAmount) {
+		this.age = age;
+		this.name = name;
+		this.weight = weight;
+		this.limbAmount = limbAmount;
+	}
+	move() {
+		console.log('I can run and climb trees');
+	}
+	say() {
+		console.log('I can speak and scream');
+	}
+	eat() {
+		console.log('I like vegetables and fruits');
+	}
+}
+
+class Dogs extends Predators {
+	constructor(age, name, weight, limbAmount, breed) {
+		super(age, name, weight, limbAmount);
+		this.breed = breed;
+	}
+  say() {
+		console.log('I can speak gaw');
+	}
+	eat() {
+		console.log('I like bones');
+	}
+}
+const flags = {
+  writable: false,
+  enumerable: true,
+  configurable: false,
+}
+
+const markiz = new Dogs(5, 'Markiz', 20, 4, 'Pudel');
+Object.defineProperty(markiz, 'breed', flags);
+Object.defineProperty(markiz, 'say', flags);
+// Object.defineProperty(markiz, 'smart', {value: 'true',
+//                                        writable: true, 
+//                                        enumerable: true,
+//                                       configurable: true});
+Object.defineProperties(markiz, {
+  smart: {value: true, writable: true},
+  height: {value: 30, enumerable: true }
+})
+//  delete markiz.breed;
+console.log(markiz);
+for (const prop in markiz) {
+  
+    console.log(`${prop} is ${markiz[prop]}`);
+  
+}
+const obj = {
+  fName: 'vas',
+  age: 25,
+}
+console.log(Object.getOwnPropertyDescriptor(markiz, 'breed'));
+
+const mark1 = Object.assign({}, markiz);
+console.log(mark1);
+console.log(Object.getOwnPropertyDescriptor(mark1, 'breed'));
+const mark2 = Object.defineProperties({}, 
+      Object.getOwnPropertyDescriptors(markiz));
+console.log(Object.getOwnPropertyDescriptors(mark2));
